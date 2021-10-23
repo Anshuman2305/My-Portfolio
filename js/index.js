2aa2444f901d4957f1cd08a85f572754a5cd6c2c
Anshuman2305 <Anshuman.nitrr@gmail.com>
2021-10-23 19:27:58 +0530

    First Update

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+  $.scrollIt({
+  easing: 'ease-in-out',      // the easing function for animation
+  scrollTime: 1500,       // how long (in ms) the animation takes
+  });
+});
+
+AOS.init({disable: 'mobile'});
